76e6c89ead7d09cb692f80619b4c26fdd16e5f78
QMinh <quangminhpham309@gmail.com>
2026-04-22 17:48:30 +0700

    fix: update cloudinary to upload image

--- be/src/modules/cloudinary/cloudinary.provider.ts
+++ be/src/modules/cloudinary/cloudinary.provider.ts
@@ -1,14 +1,14 @@
-// cloudinary.provider.ts
-
+import { ConfigService } from "@nestjs/config";
 import { v2 as cloudinary } from "cloudinary";
 
 export const CloudinaryProvider = {
 	provide: "CLOUDINARY",
-	useFactory: () => {
+	inject: [ConfigService],
+	useFactory: (config: ConfigService) => {
 		return cloudinary.config({
-			cloud_name: process.env.CLOUDINARY_NAME,
-			api_key: process.env.CLOUDINARY_API_KEY,
-			api_secret: process.env.CLOUDINARY_API_SECRET,
+			cloud_name: config.get<string>("CLOUDINARY_CLOUD_NAME"),
+			api_key: config.get<string>("CLOUDINARY_API_KEY"),
+			api_secret: config.get<string>("CLOUDINARY_API_SECRET"),
 		});
 	},
 };
